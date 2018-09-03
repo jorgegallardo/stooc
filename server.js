13e23ab964c194128a13640c719d0ed6151081b3
Jorge Gallardo <jorge8486@gmail.com>
2018-09-03 18:34:50 -0400

    added react, bootstrap, and fontawesome

--- server.js
+++ server.js
@@ -33,6 +33,6 @@ app.get('/', (req, res) => res.send('yoo'));
 app.use('/api/auth', auth);
 app.use('/api/dashboard', dashboard);
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000.');
+app.listen(5000, () => {
+  console.log('Server started on port 5000.');
 });
